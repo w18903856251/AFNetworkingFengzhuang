01818c76d265ccdcd334b3798223a74d017ede72
吴宇 <wuyu@zhangqians-macbook-pro.local>
2016-11-25 10:49:14 +0800

    Initial Commit

---
+++ AFNetworking封装/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AFNetworking封装
+//
+//  Created by zhangqian on 16/11/25.
+//  Copyright © 2016年 Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AFNetworking封装/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AFNetworking封装
+//
+//  Created by zhangqian on 16/11/25.
+//  Copyright © 2016年 Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
